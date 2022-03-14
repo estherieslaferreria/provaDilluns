bf3461026c7706f73d6cbcdf320bf19fc64ddcbd
estherieslaferreria <esther@LAPTOP-2M7EONU8>
2022-03-14 14:11:36 +0100

    afegit hello world!! doble

--- mavenproject1/src/main/java/m5dam/mavenproject1/Prova.java
+++ mavenproject1/src/main/java/m5dam/mavenproject1/Prova.java
@@ -10,5 +10,8 @@ package m5dam.mavenproject1;
  * @author esther
  */
 public class Prova {
-    
+    public static void mostrar(){
+    System.out.println("nova modificacio");
+    System.out.println("hello   !!! world");
+    }
 }
